efc6bba643c6d03c9c1a613b4654fa82e3661af0
sj-benzyinfotech <jain.shashank@benzyinfotech.com>
2024-05-08 13:08:22 +0000

    DocType

---
+++ rentals/rental/doctype/driver/test_driver.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, sj and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDriver(FrappeTestCase):
+	pass
---
+++ rentals/rental/doctype/vehicle/test_vehicle.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, sj and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestVehicle(FrappeTestCase):
+	pass
